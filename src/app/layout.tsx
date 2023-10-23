a36ef3d6eb96c3f010743bf68fd540768e0433be
templar-ajay <templar.ajay@gmail.com>
2023-10-23 20:36:28 +0530

    feat: add prismic preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import clsx from "clsx";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-import { createClient } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -52,6 +53,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50"></div>
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
